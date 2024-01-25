f7347dfca393ca9ee6b1d0ae7716744fbaf912b4
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-01-25 13:10:03 +0530

    connected to database

--- Backend/index.js
+++ Backend/index.js
@@ -6,3 +6,13 @@ app.listen(PORT, ()=>{
     console.log(`Server is running at port no. ${PORT}`)
 })
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.URL)
+    .then(()=>{
+        console.log("DB connected successfully...")
+    })
+    .catch((error) => {
+        console.log(error);
+        console.log("DB connection is facing issue...");
+    })
