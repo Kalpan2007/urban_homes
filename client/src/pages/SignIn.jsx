8a0d729a7ad41cb19fd37bb7d9bee6ec9b859b0d
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-01-30 15:53:49 +0530

    create and add google auth component OAuth Added

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {Link, Navigate, useNavigate} from "react-router-dom"
 import { useDispatch, useSelector } from 'react-redux';
 import {signInStart, signInSuccess, signInFailure} from "../redux/user/userSlice"
+import OAuth from '../components/OAuth';
 
 export default function Signln() {
   const dispatch = useDispatch();
@@ -66,6 +67,7 @@ export default function Signln() {
         <button disabled={loading} className='bg-black font-bold text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? 'Loading...' : 'Sign in'}
         </button>
+        <OAuth />
       </form>
 
       <div className='flex gap-2 mt-5'>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {Link} from "react-router-dom"
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
 
@@ -61,6 +62,7 @@ export default function SignUp() {
         <button disabled={loading} className='bg-black font-bold text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? 'Loading...' : 'Sign up'}
         </button>
+        <OAuth />
       </form>
 
       <div className='flex gap-2 mt-5'>
