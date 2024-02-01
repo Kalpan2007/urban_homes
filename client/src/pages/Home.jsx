f95e7dd2661daae1f44b409298bcbbbd156c2e7a
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-02-01 10:51:18 +0530

    Home UI  page updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className='max-w-6xl mx-auto flex'>
-      <div className='flex flex-col justify-center'>
-        <h2 className='font-bold text-wrap text-xxl'>Find your next <span>perfect</span><br></br>
+    <div className='scrolling-sign overflow-y-hidden'>
+    
+      <div className='flex gap-6 max-w-8xl max-auto'>
+        <div className='flex flex-col gap-6 py-28 px-[150px] '>
+          <h1 className='text-black text-3xl font-bold lg: text-5xl'>
+            Find your next <span className='text-[#161A76]'>perfect</span><br/>
             place with ease
-        </h2>
-        <p>
-        Urban Homes will help you find your home fast, easy and comfortable.<br/>
-        Our expert support are always available,
-        </p>
-        <button>Let's Start now...</button>
+          </h1>
+          <div className='text-black text-xs sm:text-sm'>
+            Urban Homes will help you find your next perfect place to live, 
+            <br/>easy and comfortable.
+          <br/> Our expert support are always available
+
+          </div>
+
+          <Link to="/search" className='text-xs sm:text-sm text-[#161A76] font-bold hover:underline'>
+            let's get started...
+          </Link>
+        </div>
+        
+
+        <div>
+          <img className='' src='https://urban-homes.s3.ap-south-1.amazonaws.com/Smart+home+system+in+the+room.png'></img>
+        </div>
       </div>
 
-      <div>
-        <img src='https://urban-homes.s3.ap-south-1.amazonaws.com/Smart+home+system+in+the+room.png'></img>
-      </div>
+      <img className='w-full h-[800px]' src='https://urban-homes.s3.ap-south-1.amazonaws.com/full-image.png'></img>
     </div>
   )
 }
