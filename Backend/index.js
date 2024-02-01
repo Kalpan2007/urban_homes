551af42476f25dd8ab37b6533c68adbe97f85d6f
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-02-01 23:22:20 +0530

    update user route is created

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 const cors = require('cors');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(cors());
 
 
 app.use(express.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 require('dotenv').config();
