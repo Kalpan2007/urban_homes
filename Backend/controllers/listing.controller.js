4eb641de2fb0685c7ec089aa436b0a18a5a2f3e1
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-02-04 23:42:32 +0530

    updating listing page UI and functionality is done

--- Backend/controllers/listing.controller.js
+++ Backend/controllers/listing.controller.js
@@ -48,4 +48,17 @@ exports.updateListing = async (req,res,next) =>{
   } catch (error) {
     next(error);
   }
+};
+
+exports.getListing = async (req,res,next)=>{
+  try {
+    const listing = await Listing.findById(req.params.id);
+    // console.log(listing)
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
 }
